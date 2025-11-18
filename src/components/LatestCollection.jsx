b51f72d6ad57be0937b35794c38f2a5ec89352b6
Sidhartha_sahani <sidharthasahanii@gmail.com>
2025-11-18 21:58:08 +0545

    Vape Station

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -16,7 +16,7 @@ const LatestCollection = () => {
       <div className="text-center py-8 text-3xl">
         <Title text1={"LATEST"} text2={"COLLECTION"}></Title>
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+           Officia
           temporibus nostrum ipsum fuga accusamus, eligendi dicta voluptate
           neque at? Dolor?
         </p>
