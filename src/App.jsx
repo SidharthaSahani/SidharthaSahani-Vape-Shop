978d9f2f5297b64773b29309d3f929375d3a75f7
Sidhartha_sahani <sidharthasahanii@gmail.com>
2025-11-28 09:35:17 +0545

    search bar fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Cart from './pages/Cart'
 import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
 
