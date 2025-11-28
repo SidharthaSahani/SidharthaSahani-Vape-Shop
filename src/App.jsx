99315d6b18fafaf2554d9b06195fecd5af1e669b
Sidhartha_sahani <sidharthasahanii@gmail.com>
2025-11-28 09:40:46 +0545

    search bar fix

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
+import Newsletterbox from './components/Newsletterbox'
 
 const App = () => {
   return (
